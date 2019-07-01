e87b69772be1d67c80f6480b1ab4c7749bd96141
Gergő Kovács <gergo.kovacs.dev@gmail.com>
2019-07-01 10:04:25 +0200

    Updated version to 1.0.0-SNAPSHOT

--- src/main/java/hu/eteosf/gergokovacs/userorders/repository/UserRepository.java
+++ src/main/java/hu/eteosf/gergokovacs/userorders/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package hu.eteosf.gergokovacs.userorders.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 import hu.eteosf.gergokovacs.userorders.model.entity.UserEntity;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findByUserId(String userId);
 }
